3633ed9bc626637a83b298eaec08a66d5b57d3e1
Jesús <j2a0a0a5@gmail.com>
2023-10-06 02:33:10 -0400

    alguna parte del codigo
    algo de movimiento en el code jeje

--- Goofy game/Assets/Codiguitos/Movimientoo.cs
+++ Goofy game/Assets/Codiguitos/Movimientoo.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 public class Movimientoo : MonoBehaviour
 {
     public Rigidbody2D cuerpesito {get; private set;}
+    private Vector2 direction = Vector2.down;
+    public float speed = 5f;
+
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputDown = KeyCode.S;
+    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputRight = KeyCode.D;
     
     private void Awake(){
-        cuerpesito = GetComponent<Rigidbody2D>();
+        cuerpesito   = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update(){
+        if(Input.GetKey(inputUp)){
+            asignarDireccion(Vector2.up);
+        } else if (Input.GetKey(inputDown)){
+            asignarDireccion(Vector2.down);
+        } else if (Input.GetKey(inputLeft)){
+            asignarDireccion(Vector2.left);
+        } else if (Input.GetKey(inputRight)){
+            asignarDireccion(Vector2.right);
+        } else{
+            asignarDireccion(Vector2.zero);
+        }
+    }
+
+    private void FixedUpdate(){
+        Vector2 posicion = cuerpesito.position;
+        Vector2 traslacion = direction * speed * Time.fixedDeltaTime;
+
+        cuerpesito.MovePosition(posicion + traslacion);
+    }
+    
+    private void asignarDireccion(Vector2 nuevaDireccion){
+        direction = nuevaDireccion;
     }
 }
