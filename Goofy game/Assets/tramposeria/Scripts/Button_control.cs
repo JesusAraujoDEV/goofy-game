0b471b6178d3e857c727b27536251dfd2bee88a7
laichangdiaz <laichangdiaz@gmail.com>
2023-10-18 23:25:14 -0400

    conexion scene

---
+++ Goofy game/Assets/tramposeria/Scripts/Button_control.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void PlayGame()
+    //Boton que pasa de start a menu
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+    public void QuitGame()
+    //Noton para salir del juego
+    {
+        Application.Quit();
+    }
+}
