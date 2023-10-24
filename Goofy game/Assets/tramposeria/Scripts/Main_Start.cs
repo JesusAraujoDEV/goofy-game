77554149b1bf6f479e25f4f7e6cf3c6f65077d12
Jesús <j2a0a0a5@gmail.com>
2023-10-23 22:42:35 -0400

    Arreglando DESCUIDOS!!!!!! DE ALGUIEN
    Por favor siempre abre primero github antes que unity

---
+++ Goofy game/Assets/tramposeria/Scripts/Main_Start.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class Main_Start : MonoBehaviour
+{
+    
+    public void StartGame()
+    //Boton que pasa de start a menu
+    {
+        SceneManager.LoadScene("menu");
+    }
+    public void SkinGame()
+    {
+        SceneManager.LoadScene("skin");
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("play_mode");
+    }
+    public void QuitGame()
+    //Noton para salir del juego
+    {
+        Application.Quit();
+    }    
+    
+}
