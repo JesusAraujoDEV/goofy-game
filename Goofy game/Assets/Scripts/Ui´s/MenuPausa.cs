0020ea96d8620977179232512546dca27c45567e
Jesús <j2a0a0a5@gmail.com>
2023-10-28 18:19:52 -0400

    algo habré hecho

--- Goofy game/Assets/Scripts/Ui´s/MenuPausa.cs
+++ Goofy game/Assets/Scripts/Ui´s/MenuPausa.cs
@@ -7,6 +7,8 @@ public class MenuPausa : MonoBehaviour
 {
     [SerializeField] private GameObject pauseButton;
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject pauseWinner;
+    public PlumasManager plumasManager;
 
     private bool isPaused = false;
 
@@ -23,6 +25,23 @@ public class MenuPausa : MonoBehaviour
                 Pause();
             }
         }
+        string scene = SceneManager.GetActiveScene().name;
+
+        if (scene == "Plumas"){
+            int cantidadPlumasWhite = plumasManager.PlumasTotalesWhiteDuck;
+            int cantidadPlumasBlack = plumasManager.PlumasTotalesBlackDuck;
+            if(cantidadPlumasBlack == 5 || cantidadPlumasWhite == 5){
+                int mayor = plumasManager.PlumasTotalesWhiteDuck;
+                int menor = plumasManager.PlumasTotalesBlackDuck;
+                if (mayor < menor){
+                    mayor = plumasManager.PlumasTotalesBlackDuck;
+                    menor = plumasManager.PlumasTotalesWhiteDuck;
+                }
+            StatusWinner();    
+            }
+        
+        }
+    
     }
 
     //Detener tiempo del juego
@@ -55,5 +74,10 @@ public class MenuPausa : MonoBehaviour
     {
         Debug.Log("Closing game");
         Application.Quit();
+    }
+
+    public void StatusWinner(){
+        Time.timeScale = 0f;
+        pauseWinner.SetActive(true);
     }
 }
