71b81c49c514434ff626b8d88e3b666f39627021
laichangdiaz <laichangdiaz@gmail.com>
2023-10-29 10:51:54 -0400

    menu quit y babes

--- Goofy game/Assets/Scripts/Ui´s/MenuPausa.cs
+++ Goofy game/Assets/Scripts/Ui´s/MenuPausa.cs
@@ -8,6 +8,9 @@ public class MenuPausa : MonoBehaviour
     [SerializeField] private GameObject pauseButton;
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject pauseWinner;
+    [SerializeField] private GameObject quitMenu;
+    
+    [SerializeField] private GameObject panelMenu;
     public PlumasManager plumasManager;
 
     private bool isPaused = false;
@@ -70,8 +73,29 @@ public class MenuPausa : MonoBehaviour
     }
 
     //cerrar juego 
+
     public void Quit()
     {
+        quitMenu.SetActive(true);
+        panelMenu.SetActive(false);
+    }
+
+    //volver a menu de pausa
+    public void ReturnMenuPause()
+    {
+        quitMenu.SetActive(false);
+        panelMenu.SetActive(true);
+    }
+    public void QuitPlay()
+    {
+        //salir de la partida
+            SceneManager.LoadScene("menu");
+    
+    }
+
+    public void QuitGame()
+    {
+        //salir del juego
         Debug.Log("Closing game");
         Application.Quit();
     }
